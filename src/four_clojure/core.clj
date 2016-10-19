3af2efca3a53bbf9acd0e53021ca874c1bfb1e63
geekerzp <geekerzp@gmail.com>
2016-10-19 22:46:49 +0800

    49

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -4,3 +4,10 @@
 ;; http://www.4clojure.com/problem/41
 (fn [coll n]
   (mapcat #(take (dec n) %) (partition-all n coll)))
+
+;; Split a sequence
+;; http://www.4clojure.com/problem/49
+(fn [n coll]
+  [(take n coll) (drop n coll)])
+
+(juxt take drop)
