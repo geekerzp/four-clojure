ebe4a8c48a2d31d95b1aa28e1bd020d629a50d98
geekerzp <geekerzp@gmail.com>
2016-11-11 18:05:48 +0800

    refactor

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -2,24 +2,28 @@
 
 ;; Drop Every Nth Element
 ;; http://www.4clojure.com/problem/41
-(fn [coll n]
-  (mapcat #(take (dec n) %) (partition-all n coll)))
+(def drop-every-nth-element
+ (fn [coll n]
+   (mapcat #(take (dec n) %) (partition-all n coll))))
 
 ;; Split a sequence
 ;; http://www.4clojure.com/problem/49
-(fn [n coll]
-  [(take n coll) (drop n coll)])
+(def split-a-sequence
+ (fn [n coll]
+   [(take n coll) (drop n coll)]))
 
 (juxt take drop)
 
 ;; Advanced Destructuring
-;; https://www.4clojure.com/problem/51
-[1 2 3 4 5]
+;; https://www.4clojure.com/problem/
+(def advanced-destructuring
+  [1 2 3 4 5])
 
 ;; A Half-Truth
 ;; https://www.4clojure.com/problem/83
-(fn [& args]
-  (and (boolean (some true? args)) (not-every? true? args)))
+(def a-half-truth
+ (fn [& args]
+   (and (boolean (some true? args)) (not-every? true? args))))
 
 ;; Map Construction
 ;; https://www.4clojure.com/problem/61
